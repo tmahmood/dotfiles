337f71f7b3ba44e2a762c25632589f01fdc3b9a9
Tarin Mahmood <34904+tmahmood@users.noreply.github.com>
2024-10-07 04:44:09 +0600

    Updates

--- arch.sh
+++ arch.sh
@@ -16,10 +16,12 @@ grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 pacman -S --noconfirm gnome gnome-extra i3 rsync sudo visudo zsh rofi
-pacman -S --noconfirm --needed git base-devel networkmanager
+pacman -S --noconfirm --needed git base-devel networkmanager cmake pacman-contrib
+
 
 useradd -m mahmood -G wheel
 passwd mahmood
+chsh -s /usr/bin/zsh mahmood
 # Need to enable wheel in sudoer file
 echo "Please enable sudo in sudoer file, creating the user based install script"
 
@@ -33,40 +35,17 @@ cd repo
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 yay -Y --gendb
 
+git clone https://github.com/tmahmood/dotfiles
+ln -sf ~/repo/dotfiles/zshrc ~/.zshrc
+
 alias yaync=yay -S --noconfirm
 
-yaync i3-gnome 
-yaync polybar
-yaync dropbox
-yaync intellij-idea-ultimate-edition
-yaync firfox-nightly-bin
-yaync ttf-martian-mono
-yaync ttf-maple
-yaync copyq
-yaync feh
-yaync flameshot
-yaync keepassxc
-yaync wezterm
-yaync atuin
-yaync starship
-yaync broot
-yaync task
+yaync i3-gnome polybar dropbox jdk-openjdk intellij-idea-ultimate-edition firfox-nightly-bin ttf-martian-mono ttf-maple copyq feh flameshot keepassxc wezterm atuin starship broot task betterlockscreen dunst dex fzf ripgrep gnome-pomodoro syncthing yazi nyxt libreoffice-fresh obsidian jq p7zip ffmpegthumbnailer fd zoxide chafa tmux-plugin-manager ttf-martian-mono-nerd
+
 rustup toolchain install nightly
 
 ## NEXT
-yaync betterlockscreen
-yaync dunst
-yaync dex
-yaync fzf
-yaync ripgrep
-yaync gnome-pomodoro
-yaync syncthing
-yaync yazi
-yaync nyxt
-yaync libreoffice-fresh
-yaync obsidian
 
-git clone https://github.com/tmahmood/dotfiles
 git clone https://github.com/tmahmood/taskwarrior-web
 git clone https://github.com/tmahmood/bing-wall-rs
 
@@ -83,14 +62,24 @@ cd ~/repo/taskwarrior-web/
 target/release/taskwarrior-web
 EOF_IN
 
+cat << EOF_IN > /home/mahmood/bin/polybar.sh
+#!/bin/bash
+
+polybar-msg cmd quit
+# Launch bar1 and bar2
+polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+EOF_IN
+
+
 ln -sf ~/repo/dotfiles/scripts/reboot_needed ~/bin/reboot_needed
 ln -sf ~/repo/dotfiles/scripts/change_wall.sh ~/bin/change_wall.sh
-ln -sf ~/repo/tmux/tmux.conf ~/.tmux.conf
+ln -sf ~/repo/dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -sf ~/repo/dotfiles/wezterm ~/.config/
 ln -sf ~/repo/dotfiles/dunst ~/.config/dunst
 ln -sf ~/repo/dotfiles/vim_bundle ~/.vim
+
 vim +'PlugInstall --sync' +qa
-EOF
+
 if [[ $1 == 'crossroads' ]]; then
     echo 'crossroads'
     mkdir .config/i3/
@@ -102,3 +91,4 @@ elif [[ $1 == 'tmndesk' ]]; then
     ln -sf ~/repo/dotfiles/tmahmood/polybar ~/.config/polybar
 fi
 
+EOF
