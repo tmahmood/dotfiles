076b3b43e1244fecb4724dfb03b0c9f999e8cde0
Tarin Mahmood <34904+tmahmood@users.noreply.github.com>
2025-04-05 05:41:08 +0600

    Added wm

--- gnome/export.sh
+++ gnome/export.sh
@@ -6,3 +6,4 @@
 dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > keybindings.txt
 dconf dump /org/gnome/mutter/keybindings/ > mutter-keybindings.txt
 dconf dump /org/gnome/shell/keybindings/ > shell-keybindings.txt
+dconf dump /org/gnome/desktop/wm/keybindings/ > wm-keybindings.txt
--- gnome/import.sh
+++ gnome/import.sh
@@ -6,3 +6,4 @@
 dconf load /org/gnome/settings-daemon/plugins/media-keys/ < keybindings.txt
 dconf load /org/gnome/mutter/keybindings/ < mutter-keybindings.txt
 dconf load /org/gnome/shell/keybindings/ < shell-keybindings.txt
+dconf load /org/gnome/desktop/wm/keybindings/ < wm-keybindings.txt
