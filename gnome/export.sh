2ced639466be97e3f1915374847810b84b184ef1
Tarin Mahmood <34904+tmahmood@users.noreply.github.com>
2025-04-05 05:23:27 +0600

    Fixed import/export script

--- gnome/export.sh
+++ gnome/export.sh
@@ -3,6 +3,6 @@
 # export gnome settings
 
 
-dconf load /org/gnome/settings-daemon/plugins/media-keys/ < keybindings.txt
-dconf load /org/gnome/mutter/keybindings/ < mutter-keybindings.txt
-dconf load /org/gnome/shell/keybindings/ < shell-keybindings.txt
+dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > keybindings.txt
+dconf dump /org/gnome/mutter/keybindings/ > mutter-keybindings.txt
+dconf dump /org/gnome/shell/keybindings/ > shell-keybindings.txt
