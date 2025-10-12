1c4e3e559b7d3df58b3c44ac6c79f55dd06880ea
Tarin Mahmood <34904+tmahmood@users.noreply.github.com>
2025-10-12 13:33:41 +0600

    Updated few things

--- scripts/change_wall.sh
+++ scripts/change_wall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
 
-~/bin/bing-wall
+#~/bin/bing-wall
 
-find $HOME/Pictures/Wallpapers/ /usr/share/backgrounds $HOME/Pictures/BingWallpaper -type f | shuf -n1 --output ~/tmp/wallpaper.txt 
+# find $HOME/Pictures/Wallpapers/ /usr/share/backgrounds $HOME/Pictures/BingWallpaper -type f | shuf -n1 --output ~/tmp/wallpaper.txt 
 
 disp_bk=$DISPLAY
 USR_DISPLAY=$(cat /proc/$$/environ | tr '\0' '\n' | grep '^DISPLAY=' | cut -c 9-)
@@ -11,6 +11,6 @@ XX=$(awk --field-separator '\n' '{print "\x27"$1"\x27"}' ~/tmp/wallpaper.txt)
 echo "$XX" | DISPLAY="$USR_DISPLAY" xargs betterlockscreen -u
 echo "$XX" | DISPLAY="$USR_DISPLAY" xargs feh --bg-fill
 DISPLAY=$disp_bk
-echo "$XX"
+#echo "$XX"
 # generates the color
 # wal -s -t -n -i "$XX"
