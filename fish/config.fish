2ce51d7837a08b550e1b66f55dcc1b54ad41d2ba
Tarin Mahmood <34904+tmahmood@users.noreply.github.com>
2025-12-15 20:17:14 +0600

    NIRI fixes

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
     starship init fish | source
+    if [ -n "$TMUX" ] && [ -n "$DIRENV_DIR" ]; then
+        unset "$DIRENV_*"  # unset env vars starting with DIRENV_
+    end
     direnv hook fish | source
     atuin init fish | source
     set -g TERM xterm-256color
@@ -15,6 +18,8 @@ if status is-interactive
     alias cat=bat
     alias pkgi="yay -Qq | fzf --preview 'yay -Qil {}' --height=97% --layout=reverse --bind 'enter:execute(yay -Qil {} | less)'"
     fish_add_path "/home/mahmood/.local/bin"
+    # ensure compatibility tmux <-> direnv
+
 
     set -g fish_key_bindings fish_vi_key_bindings
 end
