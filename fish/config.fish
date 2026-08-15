9f3aa347180f8662282c26a2ce97d9d4f9564208
Tarin Mahmood <34904+tmahmood@users.noreply.github.com>
2026-08-15 18:35:45 +0600

    Added radicale

--- fish/config.fish
+++ fish/config.fish
@@ -9,7 +9,7 @@ if status is-interactive
     direnv hook fish | source
     atuin init fish | source
     set -g TERM xterm-256color
-    alias ls='eza -F=always --icons always --color-scale'
+    alias ls='eza -F=always --icons always'
     alias cat=bat
     alias pkgi="yay -Qq | fzf --preview 'yay -Qil {}' --height=97% --layout=reverse --bind 'enter:execute(yay -Qil {} | less)'"
     set -g fish_key_bindings fish_vi_key_bindings
@@ -19,4 +19,7 @@ set -gx PATH $PATH ~/.cargo/bin
 set -gx TERMINAL ghostty
 set -gx EDITOR vim
 set -gx VCPKG ~/vcpkg
+set -gx ANDROID_HOME $HOME/apps/Android/Sdk
 set -U fish_user_paths $fish_user_paths /home/mahmood/.local/bin
+set -U fish_user_paths $fish_user_paths $ANDROID_HOME/emulator
+set -U fish_user_paths $fish_user_paths $ANDROID_HOME/platform-tools
--- scripts/change_wall.sh
+++ scripts/change_wall.sh
@@ -3,14 +3,24 @@
 
 #~/bin/bing-wall
 
-# find $HOME/Pictures/Wallpapers/ /usr/share/backgrounds $HOME/Pictures/BingWallpaper -type f | shuf -n1 --output ~/tmp/wallpaper.txt 
+find "$HOME/Pictures/Wallpapers/" "/usr/share/backgrounds" "$HOME/Pictures/BingWallpaper" -type f \
+    | shuf -n1 --output ~/tmp/wallpaper.txt 
 
 disp_bk=$DISPLAY
 USR_DISPLAY=$(cat /proc/$$/environ | tr '\0' '\n' | grep '^DISPLAY=' | cut -c 9-)
 XX=$(awk --field-separator '\n' '{print "\x27"$1"\x27"}' ~/tmp/wallpaper.txt)
-echo "$XX" | DISPLAY="$USR_DISPLAY" xargs betterlockscreen -u
 echo "$XX" | DISPLAY="$USR_DISPLAY" xargs feh --bg-fill
+
+tmp_file=~/tmp/genwall
+if [[ -f $tmp_file ]]; then
+    exit
+fi
+touch $tmp_file
+
+echo "$XX" | DISPLAY="$USR_DISPLAY" xargs betterlockscreen -u
 DISPLAY=$disp_bk
+
+rm $tmp_file
 #echo "$XX"
 # generates the color
 # wal -s -t -n -i "$XX"
